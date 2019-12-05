21de0e99b1627b209dd6e77b118b76fe76198dab
Damian Borowy <tymooteuusz@gmail.com>
2019-12-05 20:54:45 +0100

    Changed .NET version to 3.1 and updated packages

--- Server/Services/NhibernateService.cs
+++ Server/Services/NhibernateService.cs
@@ -1,36 +1,24 @@
 ﻿using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
-using Microsoft.AspNetCore.Hosting;
 using NHibernate;
 using NHibernate.Tool.hbm2ddl;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 using YourNote.Shared.Models.MappingClasses;
-using System.Data.SqlClient;
 using YourNote.Server.Models.MappingClasses;
 
 namespace YourNote.Server.Services
 {
     public class NhibernateService
     {
-
-
         // Obtain connection string information from the portal
         //
         private static string[] connectionData = GetConnectionData();
         private static string Host = connectionData[0];
         private static string Port = connectionData[1];
         private static string DBname = connectionData[2];
-        private static string User = connectionData[3]; 
-        private static string Password = connectionData[4]; 
-        
-        
-
-
-
+        private static string User = connectionData[3];
+        private static string Password = connectionData[4];
 
         public static ISessionFactory SessionFactory { get; set; }
 
@@ -38,31 +26,26 @@ namespace YourNote.Server.Services
         {
             SessionFactory = CreateSessionFactory();
         }
-       
+
         public static ISessionFactory CreateSessionFactory()
         {
-
             bool createNew = false;
-            
+
             return Fluently.Configure()
                 .Database(PostgreSQLConfiguration.PostgreSQL81
                 .ConnectionString($" Host={Host}; Port={Port}; Database={DBname};" +
-                $" Username={User}; Password={Password};"))              
+                $" Username={User}; Password={Password};"))
                 .Mappings(m => m.FluentMappings
-                .AddFromAssemblyOf<NoteMap>()
-                .AddFromAssemblyOf<UserMap>())
-                
-                .ExposeConfiguration(cfg => new SchemaExport(cfg)     
+                    .AddFromAssemblyOf<NoteMap>()
+                    .AddFromAssemblyOf<UserMap>()
+                )
+                .ExposeConfiguration(cfg => new SchemaExport(cfg)
                 .Create(createNew, createNew))
-                
                 .BuildSessionFactory();
         }
 
         public ISession OpenSession() => SessionFactory.OpenSession();
 
         private static string[] GetConnectionData() => Environment.GetEnvironmentVariable("PGPASSDATA").Split(':');
-        
-
-           
     }
 }
