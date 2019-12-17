f25b4b5fbf7b87db44388cc212b3e18f8fdb086a
Dominik <iglak1998@gmail.com>
2019-12-17 01:42:40 +0100

    dodany serwis od migracji

--- Server/Program.cs
+++ Server/Program.cs
@@ -13,28 +13,9 @@ namespace YourNote.Server
     public class Program
     {
 
-        private static string[] connectionData = GetConnectionData();
-        private static string Host = connectionData[0];
-        private static string Port = connectionData[1];
-        private static string DBname = connectionData[2];
-        private static string User = connectionData[3];
-        private static string Password = connectionData[4];
-
-        private static readonly string connectionString = $" Host={Host}; Port={Port}; Database={DBname};" +
-                $" Username={User}; Password={Password};";
-
+     
         public static void Main(string[] args)
         {
-
-            var serviceProvider = CreateServices();
-
-            // Put the database update into a scope to ensure
-            // that all resources will be disposed.
-            using (var scope = serviceProvider.CreateScope())
-            {
-                UpdateDatabase(scope.ServiceProvider);
-            }
-
 
 
 
@@ -42,34 +23,7 @@ namespace YourNote.Server
         }
 
 
-        private static IServiceProvider CreateServices()
-        {
-            return new ServiceCollection()
-                // Add common FluentMigrator services
-                .AddFluentMigratorCore()
-                .ConfigureRunner(rb => rb
-                    // Add SQLite support to FluentMigrator
-                    .AddPostgres()
-                    // Set the connection string
-                    .WithGlobalConnectionString(connectionString)
-                    // Define the assembly containing the migrations
-                    .ScanIn(typeof(AddLogTable).Assembly).For.Migrations())
-                // Enable logging to console in the FluentMigrator way
-                .AddLogging(lb => lb.AddFluentMigratorConsole())
-                // Build the service provider
-                .BuildServiceProvider(false);
-        }
-
-
-        private static void UpdateDatabase(IServiceProvider serviceProvider)
-        {
-            // Instantiate the runner
-            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
-
-            // Execute the migrations
-            runner.MigrateUp();
-        }
-
+       
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
@@ -79,8 +33,7 @@ namespace YourNote.Server
                 .UseStartup<Startup>()
                 .Build();
 
-        private static string[] GetConnectionData() => Environment.GetEnvironmentVariable("PGPASSDATA").Split(':');
-
+        
 
 
     }
---
+++ Server/Services/FluentMigratorService.cs
@@ -0,0 +1,84 @@
+﻿
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YourNote.Shared.Models;
+
+namespace YourNote.Server.Services
+{
+    public class FluentMigratorService
+    {
+        #region Connection handler
+
+        private static string[] connectionData = GetConnectionData();
+        private static string Host = connectionData[0];
+        private static string Port = connectionData[1];
+        private static string DBname = connectionData[2];
+        private static string User = connectionData[3];
+        private static string Password = connectionData[4];
+
+
+        private static readonly string connectionString = $" Host={Host}; Port={Port}; Database={DBname};" +
+                $" Username={User}; Password={Password};";
+
+        private static string[] GetConnectionData() => Environment.GetEnvironmentVariable("PGPASSDATA").Split(':');
+
+        #endregion
+
+        private static long? Version { get; set; }
+
+        public FluentMigratorService()
+        {
+
+            Version = null;
+
+           
+        }
+
+
+
+        private static void MigrateTo(long? version)
+        {
+
+            Version = version;
+            var serviceProvider = CreateServices(connectionString);
+
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+
+        private static IServiceProvider CreateServices(string connectionString)
+        {
+            return new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddPostgres()
+                    .WithGlobalConnectionString(connectionString)
+                    .ScanIn(typeof(Program).Assembly).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider(true);
+        }
+
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            
+
+            if (Version.HasValue)
+            {
+                runner.MigrateDown(Version.Value);
+            }
+            else
+            {
+                runner.MigrateUp();
+            }
+        }
+
+    }
+}
