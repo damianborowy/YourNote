50bd1dded096f3100bd89242e86bcdd3e2acf7b3
Damian Borowy <tymooteuusz@gmail.com>
2019-12-17 15:34:19 +0100

    sorting works

--- Shared/Models/Note.cs
+++ Shared/Models/Note.cs
@@ -6,7 +6,7 @@ using Newtonsoft.Json;
 
 namespace YourNote.Shared.Models
 {
-    public class Note
+    public class Note : IComparable<Note>
     {
 
         public virtual int Id { get; set; }
@@ -38,6 +38,14 @@ namespace YourNote.Shared.Models
         {
             user.SharedNotes.Remove(this);
             SharedTo.Remove(user);
+        }
+
+        public virtual int CompareTo(Note other)
+        {
+            if (other == null)
+                return 1;
+            else
+                return Id.CompareTo(other.Id);
         }
     }
 }
