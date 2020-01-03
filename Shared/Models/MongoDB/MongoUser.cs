3d28297c9f6a4e719da86fb246ff20ce3f688ebf
Dominik <iglak1998@gmail.com>
2020-01-03 18:44:34 +0100

    ehhh

---
+++ Shared/Models/MongoDB/MongoUser.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace YourNote.Shared.Models.MongoDB
+{
+    internal class MongoUser
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [Required]
+        [BsonElement("Name")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 8)]
+        public  string Password { get; set; }
+
+        public  Permission Role { get; set; } = Permission.Default;
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public string Author { get; set; }
+
+
+
+        public enum Permission
+        {
+            Default,
+            Moderator,
+            Admin
+        }
+    }
+}
